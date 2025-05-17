08cab4f0a7eae2cb95c1a55b9d4df8eefc825df4
Tojumo <tomas-46@hotmail.com.ar>
2025-05-17 00:48:26 -0300

    Se agrego un bloque de codigo que indica tu condicion sobre el bmi

--- bmi.c
+++ bmi.c
@@ -19,6 +19,22 @@ int main(int argc, char *argv[]) {
 	
 	printf("\n· Menor 18.5  - Bajo peso \n· 18.5 a 24.9 - Normal \n· 25.0 a 29.9 - Sobrepeso \n· mayor a 30  - Obesidad");
 	
+	if (indice_corporal >= 30){
+		printf("\n\nSu condición es: Obesidad");
+	}
+	
+	else if(indice_corporal >= 25){
+		printf("\n\nSu condición es: Sobrepeso");
+	}
+	
+	else if(indice_corporal >= 18.5){
+		printf("\n\nSu condición es: Normal");
+	}
+	
+	else if(indice_corporal < 18.5 ){
+		printf("\n\nSu condición es: Bajo peso");
+	}
+	
 	return 0;
 }
 
